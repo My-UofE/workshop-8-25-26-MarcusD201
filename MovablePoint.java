7087a489f5259ad21d4bcba443be113702861060
MarcusD201 <mlmd201@exeter.ac.uk>
2026-03-06 20:23:16 +0000

    tas k1 and 2

--- MovablePoint.java
+++ MovablePoint.java
@@ -4,14 +4,30 @@ public class MovablePoint implements Movable {
 	
 	// TASK: Add the constructor below
 
+public MovablePoint(int newx, newy, newxSpeed, newySpeed){
+	x=newx;
+	y=newy;
+	xSpeed=newxSpeed; 
+	ySpeed=newySpeed;
 
+}
 
 	// Implement abstract methods declared in the interface
 	@Override
 	public void moveUp() {
 		y += ySpeed; // moves y point up by increment ySpeed
 	}
+public void moveDown(){
+	y=y-ySpeed
+}
 
+public void moveLeft(){
+	x=x-xSpeed;
+}
+
+public void moveRight(){
+	x=x + xSpeed;
+}
 	// TASK: Code the other interface methods below
 
 
