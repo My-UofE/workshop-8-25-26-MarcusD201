dc33364d3742e2f0a5ea297b288c75f9f8efe0d7
MarcusD201 <mlmd201@exeter.ac.uk>
2026-03-09 17:30:32 +0000

    Movable circle class implemented

--- MovableCircle.java
+++ MovableCircle.java
@@ -4,11 +4,26 @@ public class MovableCircle implements Movable {
   private int radius;
   
   // TASK: Add the constructor here
-  
+  public MovableCircle(int x, int y, int xSpeed, int ySpeed, int newradius){
+MovablePoint center = new MovablePoint(x,y,xSpeed,ySpeed)
+
+radius= newradius;
+
+  }
   // TASK: Implement all abstract methods declared in the interface
   @Override
   public void moveUp() {
 	 center.moveUp();// move the center up
+  }
+public void moveDown() {
+	 center.moveDown();// move the center down
+  }
+public void moveRight() {
+	 center.moveRight();// move the center right
+  }
+
+public void moveLeft() {
+	 center.moveLeft();// move the center left
   }
 
 }
--- MovablePoint.java
+++ MovablePoint.java
@@ -4,7 +4,7 @@ public class MovablePoint implements Movable {
 	
 	// TASK: Add the constructor below
 
-public MovablePoint(int newx, newy, newxSpeed, newySpeed){
+public MovablePoint(int newx,int  newy,int newxSpeed,int  newySpeed){
 	x=newx;
 	y=newy;
 	xSpeed=newxSpeed; 
@@ -18,7 +18,7 @@ public MovablePoint(int newx, newy, newxSpeed, newySpeed){
 		y += ySpeed; // moves y point up by increment ySpeed
 	}
 public void moveDown(){
-	y=y-ySpeed
+	y=y-ySpeed;
 }
 
 public void moveLeft(){
@@ -29,6 +29,9 @@ public void moveRight(){
 	x=x + xSpeed;
 }
 	// TASK: Code the other interface methods below
-
+public String toString(){
+	String readOut= ("x= "+x+" y= "+y+" xSpeed= "+xSpeed+" ySpeed= "+ySpeed);
+	return readOut;
+}
 
 }
